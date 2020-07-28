da408807eed4c92ff5283db7a47f62932c3dfe14
GustavoAlef <gustalef@gmail.com>
2020-07-28 20:59:43 -0300

    Tentativa de consertar a rota de logon

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,6 +1,6 @@
 //verifica se a ong existe
 const connection = require("../database/connection");
- 
+
 module.exports = {
   async create(request, response) {
     const { id } = request.body;
@@ -13,6 +13,8 @@ module.exports = {
     if (!ong) {
       return response.status(400).json({ error: "no ong found with this ID!" });
     }
+
+    console.log("--->", id)
 
     return response.json(ong);
   },
