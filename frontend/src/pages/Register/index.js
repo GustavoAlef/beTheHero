0edd02b5f7c12b08fbf5b6643c6323f2a92ff258
GustavoAlef <gustalef@gmail.com>
2020-07-29 21:18:08 -0300

    add axios e finalizacao dos estilos das paginas

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 
+import api from "../../services/api";
 import "./styles.css";
 
 import logoImg from "../../assets/logo.svg";
 
 export default function Register() {
+
+  
+
   return (
     <div className="register-container">
       <div className="content">
@@ -16,13 +20,26 @@ export default function Register() {
           <h1>Cadastro</h1>
           <p>Faca seu cadastro, entre na plataforma e ajude pessoas.</p>
 
-          <Link className="back-link" to="/register">
+          <Link className="back-link" to="/">
             <FiArrowLeft size={16} color="#E02041" />
             Nao tenho cadastro.
           </Link>
         </section>
 
-        <form></form>
+        <form>
+          <input placeholder="nome da ONG" />
+          <input type="email" placeholder="E-mail" />
+          <input placeholder="whatsapp" />
+
+          <div className="input-group">
+            <input placeholder="cidade" />
+            <input placeholder="UF" style={{ width: 80 }} />
+          </div>
+
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
       </div>
     </div>
   );
