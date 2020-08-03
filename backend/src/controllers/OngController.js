1ee7e87124802860b95963bd3cdce0b22029002c
GustavoAlef <gustalef@gmail.com>
2020-08-03 20:50:50 -0300

    adicionando validacoes usando celebrate

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -11,7 +11,7 @@ module.exports = {
 
   async create(request, response) {
     //req body (usando desestruturacao)
-    const { name, email, whats, city, uf } = request.body;
+    const { name, email, whats, city, uf } = request.body; 
 
     const id = crypto.randomBytes(4).toString("HEX");
 
